eba5de9dc2cda1fc5951133d4c3763be04d96194
Michael Choi <michaeljkchoi@gmail.com>
2018-05-03 09:34:28 -0400

    Changed python dependency

--- postgresql.rb
+++ postgresql.rb
@@ -18,7 +18,7 @@ class Postgresql < Formula
   depends_on "openssl"
   depends_on "readline"
 
-  depends_on :python => :optional
+  depends_on "python@2" => :optional
   depends_on :python3 => :optional
 
   conflicts_with "postgres-xc",
